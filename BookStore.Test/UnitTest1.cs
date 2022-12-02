eef7106021f2b968f4f3a02cab99eb25b2d0b1e6
Katerina Gligorovska <katerinagligorovska@gmail.com>
2022-12-02 11:59:13 +0100

    Add example test

--- BookStore.Test/UnitTest1.cs
+++ BookStore.Test/UnitTest1.cs
@@ -3,8 +3,12 @@ namespace BookStore.Test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void PassingTest()
     {
-
+        Assert.Equal(4, Add(2, 2));
+    }
+    int Add(int x, int y)
+    {
+        return x + y;
     }
 }
